511ae89eeca690bdc36bef13973d91711cc645d9
Jiawei Zhao <c789a123@gmail.com>
2013-11-05 20:20:09 -0500

    add some place holders

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject closerve-contrib "0.1.0-SNAPSHOT"
+  :description "Optional modules for CloServe"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ragge/core.async "0.1.0-SNAPSHOT"]
+                 [hickory "0.5.1"]
+                 [closerve "0.1.0-SNAPSHOT"]])
